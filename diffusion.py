23625891975b11ddd1ed2274daac9cc6067fcdde
Gilad Turok <gt345@en-cc-unicorn-login-01.coecis.cornell.edu>
2026-03-19 18:08:27 -0400

    Running elbo ppl

--- diffusion.py
+++ diffusion.py
@@ -702,6 +702,18 @@ class Diffusion(L.LightningModule):
         sync_dist=True
     )
 
+    # Running average PPL (averaged in NLL space, then exponentiated)
+    running_ppl = self.metrics.valid_nlls['ppl'].compute()
+    if torch.isfinite(running_ppl):
+        self.log(
+            'val/ppl_running',
+            running_ppl,
+            on_step=True,
+            on_epoch=False,
+            prog_bar=False,
+            sync_dist=True
+        )
+
     return losses.loss
 
 
